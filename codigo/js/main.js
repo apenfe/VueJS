ccc365f3d82a32e027e039d0a10e999180076699
apenfe <adrian1414@hotmail.es>
2024-07-16 20:41:25 +0200

    Eventos y directivas for con VUE

--- codigo/js/main.js
+++ codigo/js/main.js
@@ -6,7 +6,25 @@ new Vue({
     data: {
         texto: "Hola mundo desde vue2",
         nombre: "nombre por defecto",
-        nota: 4
+        nota: 4,
+        peliculas: ['batman','los mercenarios','spiderman'],
+        frutas: [
+            {nombre: "manzana", temporada: "invierno", precio: 'bajo'},
+            {nombre: "uva", temporada: "invierno", precio: 'medio'},
+            {nombre: "fresa", temporada: "verano", precio: 'alto'}
+        ],
+        superfruta: {nombre: "mandarina", temporada: "verano", precio: 'bajo'},
+        peliculaNueva: null
+    },
+    methods: {
+        crearPelicula(){
+            alert('metodo crear pelicula --> '+this.peliculaNueva);
+            this.peliculas.unshift(this.peliculaNueva);
+            this.peliculaNueva = null;
+        },
+        borrarPelicula(indice){
+            this.peliculas.splice(indice,1);
+            alert('metodo borrar pelicula -->'+indice);
+        }
     }
-
 });
